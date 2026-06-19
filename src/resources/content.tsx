02c694ea75b74a4b04248dd05e93c6c0ebe5902d
im-your-taylor <wantmorepancakes@gmail.com>
2026-06-20 11:40:15 +1200

    update about page

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -117,24 +117,30 @@ const about: About = {
     title: "Work Experience",
     experiences: [
       {
-        company: "FLY",
-        timeframe: "2022 - Present",
-        role: "Senior Design Engineer",
+        company: "Foodring Namwon Seafood Ltd",
+        timeframe: "2024 - Present",
+        role: "Digital Marketing & Web Coordinator ",
         achievements: [
           <>
-            Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user
-            engagement and 30% faster load times.
+            Managed the corporate webpage and designed detailed product specification pages 
+            to improve user experience and visibility.
           </>,
           <>
-            Spearheaded the integration of AI tools into design workflows, enabling designers to
-            iterate 50% faster.
+            Executed digital marketing initiatives and utilized diverse design software 
+            to produce compelling online content.
           </>,
         ],
         images: [
           // optional: leave the array empty if you don't want to display images
           {
-            src: "/images/projects/project-01/cover-01.jpg",
-            alt: "Once UI Project",
+            src: "/images/projects/project-01/foodring2.png",
+            alt: "Product page design",
+            width: 16,
+            height: 9,
+          },
+          {
+            src: "/images/projects/project-01/foodring4.png",
+            alt: "Marketing page",
             width: 16,
             height: 9,
           },
@@ -160,7 +166,7 @@ const about: About = {
   },
   studies: {
     display: true, // set to false to hide this section
-    title: "Studies",
+    title: "Education",
     institutions: [
       {
         name: "Massey University",
